fa34cd9435e42ee36c3ddbca711fa972857805c0
redstarbuilders01-debug <redstarbuilders.01@gmail.com>
2026-02-05 20:41:47 +0530

    Update act 1.js

---
+++ act 1.js
@@ -0,0 +1,25 @@
+var choice=promt("Welcome to Area Calculator. \n Please Enter your Choice. \n1.Area of Rectangle. \n2.Area of Triangle. \n3.Area of Circle. \n4.Area of Parallelogram");
+if(choice ==1){
+    var l=prompt('Enter he length')
+    var b=prompt("Enter the breath")
+    var result=Number(1)*Number(b)
+    alert("the area is '+result")
+}
+      if(choice ==2){
+    var h=prompt('Enter he height')
+    var b=prompt("Enter the base")
+    var result=Number(h)*Number(b)/2
+    alert("the area is '+result")
+}
+          if(choice ==3){
+    var r=prompt('Enter the radius')
+    var result=3.14*Number(r)*Number(r)
+    alert("the area is '+result")
+}
+        if(choice ==4){
+    var l=prompt("Enter he height")
+    var b=prompt("Enter the corresponding base")
+    var result=Number(h)*Number(cb)
+    alert("the area is '+result")
+}
+        
